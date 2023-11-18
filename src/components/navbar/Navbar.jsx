126f53d10ab62734e5c8f9b49be75a927ae8dade
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-18 20:45:11 -0300

    enrutamiento link navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
       <nav className="flex items-center justify-between flex-wrap bg-white p-3">
         <div className="flex items-center flex-shrink-0 text-red-950 dark:text-red-950mr-6">
           <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
           <Link to="/home" className="font-semibold text-xl tracking-tight">Gym Entrena Salud</Link>
         </div>
@@ -38,7 +38,7 @@ export const Navbar = () => {
           <button className="flex items-center px-3 py-2 border rounded text-red-950 border-white ">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
         </div>
@@ -50,7 +50,7 @@ export const Navbar = () => {
             <Link to="/register" className="block mt-4 lg:inline-block lg:mt-0 text-red-950 dark:text-red-950 hover:text-zinc-950 mr-4">
               Register
             </Link>
-            <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 text-red-950 dark:text-red-950 hover:text-zinc-950">
+            <Link to="/contacto" className="block mt-4 lg:inline-block lg:mt-0 text-red-950 dark:text-red-950 hover:text-zinc-950">
               Contact
             </Link>
           </div>
