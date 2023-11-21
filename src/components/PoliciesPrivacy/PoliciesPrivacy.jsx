23d0f51d7e97648ee592412f2da0e0e821258094
LeandroUlloque <63380623+LeandroUlloque@users.noreply.github.com>
2023-11-20 22:11:29 -0300

    texto de politicas

--- src/components/PoliciesPrivacy/PoliciesPrivacy.jsx
+++ src/components/PoliciesPrivacy/PoliciesPrivacy.jsx
@@ -7,7 +7,37 @@ function PoliciesPrivacy() {
       <Navbar />
       <Carrousel />
 
-      <h3>tu codigo aca</h3>
+      <h3>Políticas y Privacidad
+
+        Bienvenido/a a Gym Entrena Salud, donde tu privacidad es una prioridad para nosotros. Al proporcionar tus datos personales a través de nuestro formulario de registro, reconoces y aceptas las siguientes políticas:
+
+        Recopilación de Información:
+        Recolectamos información como tu nombre, dirección de correo electrónico y datos de contacto para crear y gestionar tu cuenta en nuestro sitio web.
+
+        Uso de la Información:
+        Utilizamos tus datos para personalizar tu experiencia en Gym Entrena Salud, brindarte información relevante y mejorar nuestros servicios.
+
+        Seguridad de la Información:
+        Implementamos medidas de seguridad para proteger tus datos contra accesos no autorizados y garantizar su confidencialidad.
+
+        Cookies:
+        Utilizamos cookies y tecnologías similares para mejorar la funcionalidad del sitio web y entender cómo interactúas con nuestra plataforma.
+
+        Compartir Información:
+        No compartiremos tus datos personales con terceros sin tu consentimiento, a menos que sea necesario para cumplir con obligaciones legales.
+
+        Derechos del Usuario:
+        Tienes el derecho de acceder, corregir o eliminar tus datos personales. Puedes gestionar tus preferencias de privacidad desde tu cuenta en nuestro sitio web.
+
+        Retención de Datos:
+        Conservamos tus datos solo durante el tiempo necesario para los fines para los cuales fueron recopilados, a menos que la ley requiera lo contrario.
+
+        Al registrarte en Gym Entrena Salud, confías en nosotros tu información. Estamos comprometidos a proteger tu privacidad y proporcionarte una experiencia segura y satisfactoria. Para obtener más detalles sobre nuestras políticas de privacidad, no dudes en ponerte en contacto con nosotros.
+
+        ¡Gracias por formar parte de nuestra comunidad!
+
+        Gym Entrena Salud
+      </h3>
 
       <Footer />
     </>
